232449a9cd92fc5682d72eff6f2b1e87f9274baf
Nalin Dahyabhai <nalin@redhat.com>
2016-07-21 15:10:57 -0400

    Also add an add-names command
    Add an add-names command that merely adds new names to the list of names
    for the specified item.
    
    Signed-off-by: Nalin Dahyabhai <nalin@redhat.com>

--- cmd/oci-storage/name.go
+++ cmd/oci-storage/name.go
@@ -10,6 +10,42 @@ import (
 	"github.com/containers/storage/storage"
 )
 
+func addNames(flags *mflag.FlagSet, action string, m storage.Mall, args []string) int {
+	if len(args) < 1 {
+		return 1
+	}
+	id, err := m.Lookup(args[0])
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		return 1
+	}
+	oldnames, err := m.GetNames(id)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		return 1
+	}
+	newNames := []string{}
+	if oldnames != nil {
+		newNames = append(newNames, oldnames...)
+	}
+	if paramNames != nil {
+		newNames = append(newNames, paramNames...)
+	}
+	if err := m.SetNames(id, newNames); err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		return 1
+	}
+	names, err := m.GetNames(id)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		return 1
+	}
+	if jsonOutput {
+		json.NewEncoder(os.Stdout).Encode(names)
+	}
+	return 0
+}
+
 func setNames(flags *mflag.FlagSet, action string, m storage.Mall, args []string) int {
 	if len(args) < 1 {
 		return 1
@@ -35,6 +71,17 @@ func setNames(flags *mflag.FlagSet, action string, m storage.Mall, args []string
 }
 
 func init() {
+	commands = append(commands, command{
+		names:       []string{"add-names"},
+		optionsHelp: "[options [...]] imageOrContainerNameOrID",
+		usage:       "Add layer, image, or container name or names",
+		minArgs:     1,
+		action:      addNames,
+		addFlags: func(flags *mflag.FlagSet, cmd *command) {
+			flags.Var(opts.NewListOptsRef(&paramNames, nil), []string{"-name", "n"}, "New name")
+			flags.BoolVar(&jsonOutput, []string{"-json", "j"}, jsonOutput, "Prefer JSON output")
+		},
+	})
 	commands = append(commands, command{
 		names:       []string{"set-names"},
 		optionsHelp: "[options [...]] imageOrContainerNameOrID",
