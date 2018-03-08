02a59b03989284f02820a56e5c51f98db8306843
Chris Evich <cevich@redhat.com>
2018-03-08 10:00:43 -0500

    Update CI scripts
    * Relocate them with visible names under hack/...
    * Add support for adding additional $DISTRO's (later)
    * Add tmpfs mount inside SPC
    
    Signed-off-by: Chris Evich <cevich@redhat.com>

--- hack/spc_ci_test.sh
+++ hack/spc_ci_test.sh
@@ -1,33 +1,38 @@
 #!/bin/bash
 
-# N/B: This script is intended to be executed in an SPC by .run_ci_tests.sh
-#      Using it otherwise may result in unplesent side-effects.
-
-# Additional packages needed that aren't already in the base image
-DEBS="${DEBS:-btrfs-tools libdevmapper-dev}"
-
-export GO_VERSION="${GO_VERSION:-stable}"
-
-# Don't want to see this spam...unless it breaks
-echo
-echo "Updating/Installing: $DEBS"
-TMPFILE=$(mktemp)
-set +e
-(
-    set -x
-    apt-get -qq update && apt-get install -qq $DEBS
-) &> $TMPFILE
 set -e
-if [[ "$?" -gt "0" ]]
+
+if [[ "$SPC" != "true" ]] || [[ -z "$GO_VERSION" ]]
 then
-    cat $TMPFILE
+    echo "This script is intended to be executed in an SPC,"
+    echo "by run_ci_tests.sh. Using it otherwise may result"
+    echo "in unplesent side-effects."
     exit 1
 fi
 
+# Additional packages needed ontop of the base (generic) image
+case "$DISTRO" in
+    *ubuntu*)
+        export INSTALL_CMD="apt-get -qq install bats btrfs-tools libdevmapper-dev"
+        ;;
+    *)
+        echo "Unknown/unsupported \$DISTRO=$DISTRO"
+        exit 2
+        ;;
+esac
+
 echo
-echo "Setting up for go version \"$GO_VERSION\" (export GO_VERSION=something for that instead)"
+echo "Executing: $INSTALL_CMD"
+# Don't spam...unless it breaks
+TMPFILE=$(mktemp)
+$INSTALL_CMD &> $TMPFILE || ( cat $TMPFILE && exit 3 )
+rm -f "$TMPFILE"
+echo "done"
+
 if [[ ! -d "$HOME/.gimme" ]]
 then
+    echo
+    echo "Setting up for go version \"$GO_VERSION\""
     # Ref: https://github.com/travis-ci/gimme/blob/master/README.md
     mkdir -p "$HOME/bin"
     curl -sL -o $HOME/bin/gimme https://raw.githubusercontent.com/travis-ci/gimme/master/gimme
@@ -35,10 +40,11 @@ then
     # Set env. vars here and for any future bash sessions
     X=$(echo 'export GOPATH="$HOME/go"' | tee -a $HOME/.bashrc) && eval "$X"
     X=$(echo 'export PATH="${PATH}:$HOME/bin:${GOPATH//://bin:}/bin"' | tee -a $HOME/.bashrc) && eval "$X"
+    # Install go & set env vars
     X="$($HOME/bin/gimme $GO_VERSION | tee -a $HOME/.bashrc)" && eval "$X"
     unset X
 fi
-source "$HOME/.bashrc"
+source "$HOME/.bashrc"  # Just in case anything was missed
 
 echo
 echo "Build Environment:"
