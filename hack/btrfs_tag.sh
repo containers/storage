ba598e19f2b989106d909c15990ec6c865da1c01
Nalin Dahyabhai <nalin@redhat.com>
2018-08-09 13:53:42 -0400

    Disable cgo-requiring bits when cgo is not enabled
    Adjust build tags in drivers and pkg so that builds with CGO_ENABLED=0
    won't fail outright.  This ends up disabling btrfs (which uses kernel
    headers), ostree (which uses libostree), overlayfs (which uses C headers
    to define fs_disk_quota_t), and devicemapper (which uses libdevmapper
    and loopback) by default.
    
    Signed-off-by: Nalin Dahyabhai <nalin@redhat.com>

--- hack/btrfs_tag.sh
+++ hack/btrfs_tag.sh
@@ -2,9 +2,16 @@
 if test $(${GO:-go} env GOOS) != "linux" ; then
 	exit 0
 fi
-cc -E - > /dev/null 2> /dev/null << EOF
-#include <btrfs/version.h>
+cc -E - > /dev/null 2> /dev/null <<- EOF
+#include <btrfs/ioctl.h>
 EOF
 if test $? -ne 0 ; then
-	echo btrfs_noversion
+	echo exclude_graphdriver_btrfs
+else
+	cc -E - > /dev/null 2> /dev/null <<- EOF
+	#include <btrfs/version.h>
+	EOF
+	if test $? -ne 0 ; then
+		echo btrfs_noversion
+	fi
 fi
