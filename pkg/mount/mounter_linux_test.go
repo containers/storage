f71306a49017e9c05e83393130e2d704b3d9e8e4
Daniel J Walsh <dwalsh@redhat.com>
2019-05-16 16:54:53 -0400

    Fixes to allow unit test to pass on fedora 30
    Fix unit test to ignore relatime
    Update vendor to fix unit tests
    
    Signed-off-by: Daniel J Walsh <dwalsh@redhat.com>

--- pkg/mount/mounter_linux_test.go
+++ pkg/mount/mounter_linux_test.go
@@ -141,6 +141,7 @@ func validateMount(t *testing.T, mnt string, opts, optional, vfs string) {
 		}
 	}
 	volunteeredVFS := map[string]struct{}{"seclabel": {}}
+	volunteeredOPT := map[string]struct{}{"relatime": {}}
 
 	mnts := make(map[int]*Info, len(info))
 	for _, mi := range info {
@@ -171,7 +172,7 @@ func validateMount(t *testing.T, mnt string, opts, optional, vfs string) {
 		if mi.Opts != "" {
 			for _, opt := range strings.Split(mi.Opts, ",") {
 				opt = clean(opt)
-				if !has(wantedOpts, opt) && !has(pOpts, opt) {
+				if !has(volunteeredOPT, opt) && !has(wantedOpts, opt) && !has(pOpts, opt) {
 					t.Errorf("unexpected mount option %q expected %q", opt, opts)
 				}
 				delete(wantedOpts, opt)
