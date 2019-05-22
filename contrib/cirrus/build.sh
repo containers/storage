e2746fb15221270c3a376f3344ab3556f8f243d6
Daniel J Walsh <dwalsh@redhat.com>
2019-05-22 04:27:35 -0400

    Turn on vfs testing
    Signed-off-by: Daniel J Walsh <dwalsh@redhat.com>

--- contrib/cirrus/build.sh
+++ contrib/cirrus/build.sh
@@ -13,6 +13,8 @@ make STORAGE_DRIVER=overlay local-test-integration
 echo "local-test-integration Fuse-overlay"
 make STORAGE_DRIVER=overlay STORAGE_OPTION=overlay.mount_program=/usr/bin/fuse-overlayfs local-test-integration
 echo "local-test-unit"
+make STORAGE_DRIVER=vfs local-test-integration
+echo "local-test-unit"
 make local-test-unit
 
 case "$OS_REL_VER" in
@@ -21,6 +23,5 @@ case "$OS_REL_VER" in
 	make STORAGE_DRIVER=aufs local-test-integration
         ;;
 esac
-#make STORAGE_DRIVER=vfs local-test-integration
 #make STORAGE_DRIVER=overlay FUSE_OVERLAYFS_DISABLE_OVL_WHITEOUT=1 STORAGE_OPTION=overlay.mount_program=/usr/bin/fuse-overlayfs local-test-integration
 #make STORAGE_DRIVER=devicemapper STORAGE_OPTION=dm.directlvm_device=/dev/abc local-test-integration
