containers/storage

commit 0da4bc60b3babedb098dd7a75ae4ec0b75031eab

pkg/archive: strictly handle errors reading xattrs
Suggested-by: Giuseppe Scrivano <gscrivan@redhat.com>
Signed-off-by: Zac Medico <zmedico@gmail.com>

pkg/archive/archive.go
@@ -387,7 +387,10 @@ func fillGo18FileTypeBits(mode int64, fi os.FileInfo) int64 {
 // ReadSecurityXattrToTarHeader reads security.capability xattr from filesystem
 // to a tar header
 func ReadSecurityXattrToTarHeader(path string, hdr *tar.Header) error {
-	capability, _ := system.Lgetxattr(path, "security.capability")
+	capability, err := system.Lgetxattr(path, "security.capability")
+	if err != nil && err != system.EOPNOTSUPP {
+		return err
+	}
 	if capability != nil {
 		hdr.Xattrs = make(map[string]string)
 		hdr.Xattrs["security.capability"] = string(capability)
@@ -397,15 +400,21 @@ func ReadSecurityXattrToTarHeader(path string, hdr *tar.Header) error {
 
 // ReadUserXattrToTarHeader reads user.* xattr from filesystem to a tar header
 func ReadUserXattrToTarHeader(path string, hdr *tar.Header) error {
-	if xattrs, _ := system.Llistxattr(path); xattrs != nil {
+	xattrs, err := system.Llistxattr(path)
+	if err != nil && err != system.EOPNOTSUPP {
+		return err
+	}
+	if xattrs != nil {
 		for _, key := range xattrs {
 			if strings.HasPrefix(key, "user.") {
-				if value, _ := system.Lgetxattr(path, key); value != nil {
-					if hdr.Xattrs == nil {
-						hdr.Xattrs = make(map[string]string)
-					}
-					hdr.Xattrs[key] = string(value)
+				value, err := system.Lgetxattr(path, key)
+				if err != nil {
+					return err
 				}
+				if hdr.Xattrs == nil {
+					hdr.Xattrs = make(map[string]string)
+				}
+				hdr.Xattrs[key] = string(value)
 			}
 		}
 	}

pkg/archive/changes_linux.go
@@ -84,20 +84,25 @@ func walkchunk(path string, fi os.FileInfo, dir string, root *FileInfo) error {
 		return err
 	}
 	info.stat = stat
-	info.capability, _ = system.Lgetxattr(cpath, "security.capability") // lgetxattr(2): fs access
+	info.capability, err = system.Lgetxattr(cpath, "security.capability") // lgetxattr(2): fs access
+	if err != nil && err != system.EOPNOTSUPP {
+		return err
+	}
 	xattrs, err := system.Llistxattr(cpath)
-	if err != nil && err != unix.EOPNOTSUPP {
+	if err != nil && err != system.EOPNOTSUPP {
 		return err
 	}
 	if xattrs != nil {
 		for _, key := range xattrs {
 			if strings.HasPrefix(key, "user.") {
-				if value, _ := system.Lgetxattr(cpath, key); value != nil {
-					if info.xattrs == nil {
-						info.xattrs = make(map[string]string)
-					}
-					info.xattrs[key] = string(value)
+				value, err := system.Lgetxattr(cpath, key)
+				if err != nil {
+					return err
 				}
+				if info.xattrs == nil {
+					info.xattrs = make(map[string]string)
+				}
+				info.xattrs[key] = string(value)
 			}
 		}
 	}

pkg/system/xattrs_linux.go
@@ -2,8 +2,14 @@ package system
 
 import (
 	"bytes"
+	"syscall"
 
 	"golang.org/x/sys/unix"
+)
+
+const (
+	// Operation not supported
+	EOPNOTSUPP syscall.Errno = unix.EOPNOTSUPP
 )
 
 // Lgetxattr retrieves the value of the extended attribute identified by attr

pkg/system/xattrs_unsupported.go
@@ -2,6 +2,13 @@
 
 package system
 
+import "syscall"
+
+const (
+	// Operation not supported
+	EOPNOTSUPP syscall.Errno = syscall.Errno(0)
+)
+
 // Lgetxattr is not supported on platforms other than linux.
 func Lgetxattr(path string, attr string) ([]byte, error) {
 	return nil, ErrNotSupportedPlatform

commit 091491bbf78481696a8970150d9326cae11671bc

pkg/archive: limit user xattr value size to math.MaxUint16 bytes
This should avoid the following panic in Lgetxattr:

    panic: runtime error: makeslice: len out of range

Fixes: 0da4bc60b3ba ("pkg/archive: strictly handle errors reading xattrs")
See: https://travis-ci.org/containers/buildah/jobs/617652963
Signed-off-by: Zac Medico <zmedico@gmail.com>

pkg/archive/archive.go
@@ -407,6 +407,10 @@ func ReadUserXattrToTarHeader(path string, hdr *tar.Header) error {
 	for _, key := range xattrs {
 		if strings.HasPrefix(key, "user.") {
 			value, err := system.Lgetxattr(path, key)
+			if err == system.E2BIG {
+				logrus.Errorf("archive: Skipping xattr for file %s since value is too big: %s", path, key)
+				continue
+			}
 			if err != nil {
 				return err
 			}

pkg/archive/changes_linux.go
@@ -12,6 +12,7 @@ import (
 
 	"github.com/containers/storage/pkg/idtools"
 	"github.com/containers/storage/pkg/system"
+	"github.com/sirupsen/logrus"
 	"golang.org/x/sys/unix"
 )
 
@@ -95,6 +96,10 @@ func walkchunk(path string, fi os.FileInfo, dir string, root *FileInfo) error {
 	for _, key := range xattrs {
 		if strings.HasPrefix(key, "user.") {
 			value, err := system.Lgetxattr(cpath, key)
+			if err == system.E2BIG {
+				logrus.Errorf("archive: Skipping xattr for file %s since value is too big: %s", cpath, key)
+				continue
+			}
 			if err != nil {
 				return err
 			}

pkg/system/xattrs_linux.go
@@ -2,12 +2,16 @@ package system
 
 import (
 	"bytes"
+	"math"
 	"syscall"
 
 	"golang.org/x/sys/unix"
 )
 
 const (
+	// Value is larger than the maximum size allowed
+	E2BIG syscall.Errno = unix.E2BIG
+
 	// Operation not supported
 	EOPNOTSUPP syscall.Errno = unix.EOPNOTSUPP
 )
@@ -22,6 +26,9 @@ func Lgetxattr(path string, attr string) ([]byte, error) {
 		return nil, nil
 	}
 	if errno == unix.ERANGE {
+		if sz > math.MaxUint16 {
+			return nil, unix.E2BIG
+		}
 		dest = make([]byte, sz)
 		sz, errno = unix.Lgetxattr(path, attr, dest)
 	}

pkg/system/xattrs_unsupported.go
@@ -5,6 +5,9 @@ package system
 import "syscall"
 
 const (
+	// Value is larger than the maximum size allowed
+	E2BIG syscall.Errno = syscall.Errno(0)
+
 	// Operation not supported
 	EOPNOTSUPP syscall.Errno = syscall.Errno(0)
 )