containers/storage

commit 90513cbb96a7cd02ed17b72485cf326b6ac358c7

Make mount.Mounted() capable of handling files
The function `fileutils.ReadSymlinkedDirectory` makes `mount.Mounted()`
fail if the mount is a file. We now only check that if the mount is a
directory to allow handling files in `mount.Mounted()`.

Signed-off-by: Sascha Grunert <sgrunert@suse.com>

pkg/mount/mount.go
@@ -1,6 +1,7 @@
 package mount
 
 import (
+	"os"
 	"sort"
 	"strconv"
 	"strings"
@@ -56,10 +57,17 @@ func Mounted(mountpoint string) (bool, error) {
 		return false, err
 	}
 
-	mountpoint, err = fileutils.ReadSymlinkedDirectory(mountpoint)
+	info, err := os.Stat(mountpoint)
 	if err != nil {
 		return false, err
 	}
+	if info.IsDir() {
+		mountpoint, err = fileutils.ReadSymlinkedDirectory(mountpoint)
+		if err != nil {
+			return false, err
+		}
+	}
+
 	// Search the table for the mountpoint
 	for _, e := range entries {
 		if e.Mountpoint == mountpoint {

commit 4d2b7f544b59c0c81cc731faf4230f480c79385b

mount.Mounted: normalize mountpoints
Commit 90513cbb96a7 introduced a regression as specified mountpoints
must be normalized (e.g., "" -> "/") before we can deal with them.
Instead of working around the fileutils package and special casing
files, add a new `fileutils.ReadSymlinkedPath` which can work with
directories and files.

Signed-off-by: Valentin Rothberg <rothberg@redhat.com>

pkg/fileutils/fileutils.go
@@ -1,7 +1,6 @@
 package fileutils
 
 import (
-	"errors"
 	"fmt"
 	"io"
 	"os"
@@ -10,6 +9,7 @@ import (
 	"strings"
 	"text/scanner"
 
+	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 )
 
@@ -354,6 +354,21 @@ func ReadSymlinkedDirectory(path string) (string, error) {
 	}
 	if !realPathInfo.Mode().IsDir() {
 		return "", fmt.Errorf("canonical path points to a file '%s'", realPath)
+	}
+	return realPath, nil
+}
+
+// ReadSymlinkedPath returns the target directory of a symlink.
+// The target of the symbolic link can be a file and a directory.
+func ReadSymlinkedPath(path string) (realPath string, err error) {
+	if realPath, err = filepath.Abs(path); err != nil {
+		return "", errors.Wrapf(err, "unable to get absolute path for %q", path)
+	}
+	if realPath, err = filepath.EvalSymlinks(realPath); err != nil {
+		return "", errors.Wrapf(err, "failed to canonicalise path for %q", path)
+	}
+	if _, err := os.Stat(realPath); err != nil {
+		return "", errors.Wrapf(err, "failed to stat target %q of %q", realPath, path)
 	}
 	return realPath, nil
 }

pkg/mount/mount.go
@@ -1,7 +1,6 @@
 package mount
 
 import (
-	"os"
 	"sort"
 	"strconv"
 	"strings"
@@ -57,15 +56,9 @@ func Mounted(mountpoint string) (bool, error) {
 		return false, err
 	}
 
-	info, err := os.Stat(mountpoint)
+	mountpoint, err = fileutils.ReadSymlinkedPath(mountpoint)
 	if err != nil {
 		return false, err
-	}
-	if info.IsDir() {
-		mountpoint, err = fileutils.ReadSymlinkedDirectory(mountpoint)
-		if err != nil {
-			return false, err
-		}
 	}
 
 	// Search the table for the mountpoint