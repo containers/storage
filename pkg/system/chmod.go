4ab1c3e261a73c28fd043881b17c6fd31da39273
Peter Hunt <pehunt@redhat.com>
2020-10-30 12:44:44 -0400

    system/chmod: handle go <1.13
    define function isEINTR to do what errors.Is() does, but specifically to see
    if an error is syscall.EINTR
    
    Signed-off-by: Peter Hunt <pehunt@redhat.com>

--- pkg/system/chmod.go
+++ pkg/system/chmod.go
@@ -1,7 +1,6 @@
 package system
 
 import (
-	"errors"
 	"os"
 	"syscall"
 )
@@ -9,9 +8,24 @@ import (
 func Chmod(name string, mode os.FileMode) error {
 	err := os.Chmod(name, mode)
 
-	for err != nil && errors.Is(err, syscall.EINTR) {
+	for isEINTR(err) {
 		err = os.Chmod(name, mode)
 	}
 
 	return err
+}
+
+func isEINTR(err error) bool {
+	if err == nil {
+		return false
+	}
+	pathErr, ok := err.(*os.PathError)
+	if !ok {
+		return false
+	}
+	syscallErr, ok := pathErr.Err.(*syscall.Errno)
+	if !ok {
+		return false
+	}
+	return *syscallErr == syscall.EINTR
 }
