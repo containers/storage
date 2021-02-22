26834671280373f4eb79fcf9009c1fb96471e2f7
Nalin Dahyabhai <nalin@redhat.com>
2021-02-22 18:22:52 -0500

    CI: run unit tests again
    Start running unit tests again.  It was not intentionally that we
    stopped doing that.
    
    Signed-off-by: Nalin Dahyabhai <nalin@redhat.com>

--- contrib/cirrus/build_and_test.sh
+++ contrib/cirrus/build_and_test.sh
@@ -11,19 +11,19 @@ showrun make local-cross
 
 case $TEST_DRIVER in
     overlay)
-        showrun make STORAGE_DRIVER=overlay local-test-integration
+        showrun make STORAGE_DRIVER=overlay local-test-integration local-test-unit
         ;;
     fuse-overlay)
-        showrun make STORAGE_DRIVER=overlay STORAGE_OPTION=overlay.mount_program=/usr/bin/fuse-overlayfs local-test-integration
+        showrun make STORAGE_DRIVER=overlay STORAGE_OPTION=overlay.mount_program=/usr/bin/fuse-overlayfs local-test-integration local-test-unit
         ;;
     fuse-overlay-whiteout)
-        showrun make STORAGE_DRIVER=overlay FUSE_OVERLAYFS_DISABLE_OVL_WHITEOUT=1 STORAGE_OPTION=overlay.mount_program=/usr/bin/fuse-overlayfs local-test-integration
+        showrun make STORAGE_DRIVER=overlay FUSE_OVERLAYFS_DISABLE_OVL_WHITEOUT=1 STORAGE_OPTION=overlay.mount_program=/usr/bin/fuse-overlayfs local-test-integration local-test-unit
         ;;
     vfs)
-        showrun make STORAGE_DRIVER=vfs local-test-integration
+        showrun make STORAGE_DRIVER=vfs local-test-integration local-test-unit
         ;;
     aufs)
-        showrun make STORAGE_DRIVER=aufs local-test-integration
+        showrun make STORAGE_DRIVER=aufs local-test-integration local-test-unit
         ;;
     *)
         die "Unknown/Unsupported \$TEST_DRIVER=$TEST_DRIVER (see .cirrus.yml and $(basename $0))"
