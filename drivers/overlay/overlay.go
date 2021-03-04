85e960a9664f07ca9f506fe6bf3e0ad1c22dc645
Giuseppe Scrivano <gscrivan@redhat.com>
2021-03-04 15:57:34 +0100

    overlay: fix build without cgo
    Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

--- drivers/overlay/overlay.go
+++ drivers/overlay/overlay.go
@@ -630,18 +630,6 @@ func (d *Driver) Metadata(id string) (map[string]string, error) {
 	return metadata, nil
 }
 
-// ReadWriteDiskUsage returns the disk usage of the writable directory for the ID.
-// For Overlay, it attempts to check the XFS quota for size, and falls back to
-// finding the size of the "diff" directory.
-func (d *Driver) ReadWriteDiskUsage(id string) (*directory.DiskUsage, error) {
-	usage := &directory.DiskUsage{}
-	if d.quotaCtl != nil {
-		err := d.quotaCtl.GetDiskUsage(d.dir(id), usage)
-		return usage, err
-	}
-	return directory.Usage(path.Join(d.dir(id), "diff"))
-}
-
 // Cleanup any state created by overlay which should be cleaned when daemon
 // is being shutdown. For now, we just have to unmount the bind mounted
 // we had created.
