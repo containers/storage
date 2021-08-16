containers/storage

commit b4477c7a8b80ae0c2fe7aa63f20ef8b91d18cfa5

Follow symlinks if they exists
Fixes: https://github.com/containers/podman/issues/10187

Signed-off-by: Daniel J Walsh <dwalsh@redhat.com>

types/utils.go
@@ -155,8 +155,17 @@ func getRootlessUID() int {
 }
 
 func expandEnvPath(path string, rootlessUID int) (string, error) {
+	var err error
 	path = strings.Replace(path, "$UID", strconv.Itoa(rootlessUID), -1)
-	return filepath.Clean(os.ExpandEnv(path)), nil
+	path = os.ExpandEnv(path)
+	newpath, err := filepath.EvalSymlinks(path)
+	if err != nil {
+		if !os.IsNotExist(err) {
+			return "", err
+		}
+		newpath = filepath.Clean(path)
+	}
+	return newpath, nil
 }
 
 func DefaultConfigFile(rootless bool) (string, error) {

types/options_test.go
@@ -106,10 +106,8 @@ func TestGetRootlessStorageOpts2(t *testing.T) {
 	opts := StoreOptions{
 		RootlessStoragePath: "/$HOME/$UID/containers/storage",
 	}
-	storageOpts, err := getRootlessStorageOpts(2000, opts)
-
 	expectedPath := filepath.Join(os.Getenv("HOME"), "2000", "containers/storage")
-
+	storageOpts, err := getRootlessStorageOpts(2000, opts)
 	assert.NilError(t, err)
 	assert.Equal(t, storageOpts.GraphRoot, expectedPath)
 }

commit 2c2968534e931cb14d6359c1e25a9273228085ec

types: on error fallback to filepath.Clean()
Now expandEnvPath always fallbacks to filepath.Clean() when
filepath.EvalSymlinks() fails.

commit b4477c7a8b80ae0c2fe7aa63f20ef8b91d18cfa5 introduced the
regression.

This is useful in a user namespace when the permissions for the
storage path are not yet tweaked to allow access for all users.

Reported by: https://github.com/containers/buildah/pull/3450

Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

types/utils.go
@@ -160,9 +160,6 @@ func expandEnvPath(path string, rootlessUID int) (string, error) {
 	path = os.ExpandEnv(path)
 	newpath, err := filepath.EvalSymlinks(path)
 	if err != nil {
-		if !os.IsNotExist(err) {
-			return "", err
-		}
 		newpath = filepath.Clean(path)
 	}
 	return newpath, nil