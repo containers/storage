ed1006797c26c05ff2d319cbc3d762cb2906774b
Giuseppe Scrivano <gscrivan@redhat.com>
2021-10-06 12:29:23 +0200

    store: unlock store on ReloadIfChanged error
    Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

--- store.go
+++ store.go
@@ -2705,6 +2705,7 @@ func (s *store) Diff(from, to string, options *DiffOptions) (io.ReadCloser, erro
 		store := s
 		store.RLock()
 		if err := store.ReloadIfChanged(); err != nil {
+			store.Unlock()
 			return nil, err
 		}
 		if store.Exists(to) {
