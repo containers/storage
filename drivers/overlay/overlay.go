2a3194c472edf6d8fd32accc7ed74939f08621b4
Miloslav Trmač <mitr@redhat.com>
2022-02-17 18:53:04 +0100

    Don't blindly reuse state from a previous layer when re-creating it
    We have reports in the wild of a layer store where two symbolic links
    in linkDir point to the same layer. That could only happen when calling
    Driver.create with a previously-used layer ID (which happens all the time
    because pulls use deterministic layer IDs), without fully deleting
    the previous version of the layer (so far, we don't know how that has
    happened).
    
    To avoid such situations, don't just leave whatever was in
    the layer directory laying around; try to remove any pre-existing
    contents, as well as the symbolic link in linkDir, if any.
    
    Signed-off-by: Miloslav Trmač <mitr@redhat.com>

--- drivers/overlay/overlay.go
+++ drivers/overlay/overlay.go
@@ -939,6 +939,16 @@ func (d *Driver) create(id, parent string, opts *graphdriver.CreateOpts, disable
 		rootUID = int(st.UID())
 		rootGID = int(st.GID())
 	}
+
+	if _, err := system.Lstat(dir); err == nil {
+		logrus.Warnf("Trying to create a layer %#v while directory %q already exists; removing it first", id, dir)
+		// Don’t just os.RemoveAll(dir) here; d.Remove also removes the link in linkDir,
+		// so that we can’t end up with two symlinks in linkDir pointing to the same layer.
+		if err := d.Remove(id); err != nil {
+			return errors.Wrapf(err, "removing a pre-existing layer directory %q", dir)
+		}
+	}
+
 	if err := idtools.MkdirAllAndChownNew(dir, 0700, idPair); err != nil {
 		return err
 	}
