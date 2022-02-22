05075bab21f90135d687c3daed3b5826e233d392
Miloslav Trmač <mitr@redhat.com>
2022-02-22 01:32:02 +0100

    Fix a pasto from #1138
    Signed-off-by: Miloslav Trmač <mitr@redhat.com>

--- layers.go
+++ layers.go
@@ -827,7 +827,7 @@ func (r *layerStore) Put(id string, parentLayer *Layer, names []string, mountLab
 					// Either a driver error or an error saving.
 					// We now have a layer that's been marked for
 					// deletion but which we failed to remove.
-					logrus.Errorf("While recovering from a failure applying layer diff, error deleting layer %#v: %v", layer.ID, err2)
+					logrus.Errorf("While recovering from a failure saving finished layer metadata, error deleting layer %#v: %v", layer.ID, err2)
 				}
 			} else {
 				// We don't have a record of this layer, but at least
