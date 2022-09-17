2dbb0d069e6566d4ceb041b47af58902130f56a4
Doug Rabson <dfr@rabson.org>
2022-09-17 16:37:04 +0100

    Fix stat_unix_test on FreeBSD
    This splits out the platform-specific parts to stat_linux_test.go and
    stat_freebsd_test.go.
    
    Signed-off-by: Doug Rabson <dfr@rabson.org>

---
+++ pkg/system/stat_linux_test.go
@@ -0,0 +1,19 @@
+//go:build linux
+// +build linux
+
+package system
+
+import (
+	"syscall"
+	"testing"
+)
+
+// TestFromStatT tests fromStatT for a tempfile
+func platformTestFromStatT(t *testing.T, stat *syscall.Stat_t, s *StatT) {
+	if stat.Mode != s.Mode() {
+		t.Fatal("got invalid mode")
+	}
+	if stat.Mtim != s.Mtim() {
+		t.Fatal("got invalid mtim")
+	}
+}
