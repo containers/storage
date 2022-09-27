e946df9d7a01b737b1d558906a572a4954889748
Miloslav Trmač <mitr@redhat.com>
2022-09-27 20:56:25 +0200

    Deprecate API that can't return an error
    ... when we already provide variants that can do so.
    
    Signed-off-by: Miloslav Trmač <mitr@redhat.com>

--- store.go
+++ store.go
@@ -3710,9 +3710,10 @@ const AutoUserNsMaxSize = 65536
 // creating a user namespace.
 const RootAutoUserNsUser = "containers"
 
-// SetDefaultConfigFilePath sets the default configuration to the specified path
+// SetDefaultConfigFilePath sets the default configuration to the specified path, and loads the file.
+// Deprecated: Use types.SetDefaultConfigFilePath, which can return an error.
 func SetDefaultConfigFilePath(path string) {
-	types.SetDefaultConfigFilePath(path)
+	_ = types.SetDefaultConfigFilePath(path)
 }
 
 // DefaultConfigFile returns the path to the storage config file used
@@ -3722,8 +3723,9 @@ func DefaultConfigFile(rootless bool) (string, error) {
 
 // ReloadConfigurationFile parses the specified configuration file and overrides
 // the configuration in storeOptions.
+// Deprecated: Use types.ReloadConfigurationFile, which can return an error.
 func ReloadConfigurationFile(configFile string, storeOptions *types.StoreOptions) {
-	types.ReloadConfigurationFile(configFile, storeOptions)
+	_ = types.ReloadConfigurationFile(configFile, storeOptions)
 }
 
 // GetDefaultMountOptions returns the default mountoptions defined in container/storage
