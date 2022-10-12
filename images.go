9844aa39cb3156a69affa3d6b37b6df2f01b1f39
Miloslav Trmač <mitr@redhat.com>
2022-10-13 00:36:37 +0200

    Copy methods from included interfaces directly into *ImageStore
    ... so that we can modify/replace them.
    
    Should not change behavior.
    
    Signed-off-by: Miloslav Trmač <mitr@redhat.com>

--- images.go
+++ images.go
@@ -96,9 +96,47 @@ type Image struct {
 
 // roImageStore provides bookkeeping for information about Images.
 type roImageStore interface {
-	roFileBasedStore
 	roMetadataStore
 	roBigDataStore
+
+	// Acquire a writer lock.
+	// The default unix implementation panics if:
+	// - opening the lockfile failed
+	// - tried to lock a read-only lock-file
+	Lock()
+
+	// Unlock the lock.
+	// The default unix implementation panics if:
+	// - unlocking an unlocked lock
+	// - if the lock counter is corrupted
+	Unlock()
+
+	// Acquire a reader lock.
+	RLock()
+
+	// Touch records, for others sharing the lock, that the caller was the
+	// last writer.  It should only be called with the lock held.
+	Touch() error
+
+	// Modified() checks if the most recent writer was a party other than the
+	// last recorded writer.  It should only be called with the lock held.
+	Modified() (bool, error)
+
+	// TouchedSince() checks if the most recent writer modified the file (likely using Touch()) after the specified time.
+	TouchedSince(when time.Time) bool
+
+	// IsReadWrite() checks if the lock file is read-write
+	IsReadWrite() bool
+
+	// Locked() checks if lock is locked for writing by a thread in this process
+	Locked() bool
+
+	// Load reloads the contents of the store from disk.  It should be called
+	// with the lock held.
+	Load() error
+
+	// ReloadIfChanged reloads the contents of the store from disk if it is changed.
+	ReloadIfChanged() error
 
 	// Exists checks if there is an image with the given ID or name.
 	Exists(id string) bool
@@ -119,10 +157,14 @@ type roImageStore interface {
 // rwImageStore provides bookkeeping for information about Images.
 type rwImageStore interface {
 	roImageStore
-	rwFileBasedStore
 	rwMetadataStore
 	rwImageBigDataStore
 	flaggableStore
+
+	// Save saves the contents of the store to disk.  It should be called with
+	// the lock held, and Touch() should be called afterward before releasing the
+	// lock.
+	Save() error
 
 	// Create creates an image that has a specified ID (or a random one) and
 	// optional names, using the specified layer as its topmost (hopefully
