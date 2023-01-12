b4f7b119c9eec887e7ecc267a19b62770983652b
Daniel J Walsh <dwalsh@redhat.com>
2023-01-12 04:56:59 -0500

    Create pkg/regexp to better handle init regex
    The new Reexp structure, will handle the initialization of regexp
    and then will do the MustCompile within a sync.Once field.
    
    Signed-off-by: Daniel J Walsh <dwalsh@redhat.com>

---
+++ pkg/regexp/regexp_test.go
@@ -0,0 +1,29 @@
+package regexp
+
+import (
+	"testing"
+)
+
+func TestMatchString(t *testing.T) {
+	r := Delayed(`[0-9]+`)
+
+	if !r.MatchString("100") {
+		t.Fatalf("Should have matched")
+	}
+
+	if r.MatchString("test") {
+		t.Fatalf("Should not have matched")
+	}
+}
+
+func TestFindStringSubmatch(t *testing.T) {
+	r := Delayed(`a=([0-9]+).*b=([0-9]+)`)
+
+	if len(r.FindStringSubmatch("a=1,b=2")) != 3 {
+		t.Fatalf("Should have matched 3")
+	}
+
+	if len(r.FindStringSubmatch("a=1")) != 0 {
+		t.Fatalf("Should not have matched 0")
+	}
+}
