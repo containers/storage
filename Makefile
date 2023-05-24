containers/storage

commit 4e3bb7e95fb72401119f4d095177fa6420e0e6e7

Remove dependencies on ffjson
Signed-off-by: Nalin Dahyabhai <nalin@redhat.com>

Makefile
@@ -41,33 +41,15 @@ ifeq ($(shell $(GO) help mod >/dev/null 2>&1 && echo true), true)
 endif
 
 RUNINVM := vagrant/runinvm.sh
-FFJSON := tests/tools/build/ffjson
 
 default all: local-binary docs local-validate local-cross local-gccgo test-unit test-integration ## validate all checks, build and cross-build\nbinaries and docs, run tests in a VM
 
 clean: ## remove all built files
 	$(RM) -f containers-storage containers-storage.* docs/*.1 docs/*.5
 
-sources := $(wildcard *.go cmd/containers-storage/*.go drivers/*.go drivers/*/*.go pkg/*/*.go pkg/*/*/*.go) layers_ffjson.go images_ffjson.go containers_ffjson.go pkg/archive/archive_ffjson.go
-
+sources := $(wildcard *.go cmd/containers-storage/*.go drivers/*.go drivers/*/*.go pkg/*/*.go pkg/*/*/*.go)
 containers-storage: $(sources) ## build using gc on the host
 	$(GO) build $(MOD_VENDOR) -compiler gc $(BUILDFLAGS) ./cmd/containers-storage
-
-layers_ffjson.go: $(FFJSON) layers.go
-	$(RM) $@
-	$(FFJSON) layers.go
-
-images_ffjson.go: $(FFJSON) images.go
-	$(RM) $@
-	$(FFJSON) images.go
-
-containers_ffjson.go: $(FFJSON) containers.go
-	$(RM) $@
-	$(FFJSON) containers.go
-
-pkg/archive/archive_ffjson.go: $(FFJSON) pkg/archive/archive.go
-	$(RM) $@
-	$(FFJSON) pkg/archive/archive.go
 
 binary local-binary: containers-storage
 
@@ -118,7 +100,7 @@ install.tools:
 	make -C tests/tools
 
 $(FFJSON):
-	make -C tests/tools build/ffjson
+	make -C tests/tools
 
 install.docs: docs
 	make -C docs install

tests/tools/vendor/github.com/pquerna/ffjson/Makefile
@@ -1,49 +1,0 @@
-
-all: test install
-	@echo "Done"
-
-install:
-	go install github.com/pquerna/ffjson
-
-deps:
-
-fmt:
-	go fmt github.com/pquerna/ffjson/...
-
-cov:
-	# TODO: cleanup this make target.
-	mkdir -p coverage
-	rm -f coverage/*.html
-	# gocov test github.com/pquerna/ffjson/generator | gocov-html > coverage/generator.html
-	# gocov test github.com/pquerna/ffjson/inception | gocov-html > coverage/inception.html
-	gocov test github.com/pquerna/ffjson/fflib/v1 | gocov-html > coverage/fflib.html
-	@echo "coverage written"
-
-test-core:
-	go test -v github.com/pquerna/ffjson/fflib/v1 github.com/pquerna/ffjson/generator github.com/pquerna/ffjson/inception
-
-test: ffize test-core
-	go test -v github.com/pquerna/ffjson/tests/...
-
-ffize: install
-	ffjson -force-regenerate tests/ff.go
-	ffjson -force-regenerate tests/goser/ff/goser.go
-	ffjson -force-regenerate tests/go.stripe/ff/customer.go
-	ffjson -force-regenerate -reset-fields tests/types/ff/everything.go
-	ffjson -force-regenerate tests/number/ff/number.go
-
-lint: ffize
-	go get github.com/golang/lint/golint
-	golint --set_exit_status tests/...
-
-bench: ffize all
-	go test -v -benchmem -bench MarshalJSON  github.com/pquerna/ffjson/tests
-	go test -v -benchmem -bench MarshalJSON  github.com/pquerna/ffjson/tests/goser github.com/pquerna/ffjson/tests/go.stripe
-	go test -v -benchmem -bench UnmarshalJSON  github.com/pquerna/ffjson/tests/goser github.com/pquerna/ffjson/tests/go.stripe
-
-clean:
-	go clean -i github.com/pquerna/ffjson/...
-	find . -name '*_ffjson.go' -delete
-	find . -name 'ffjson-inception*' -delete
-
-.PHONY: deps clean test fmt install all

commit 47913957f88256f9e7a39a0238cf5f012092b611

Makefile: rm ffjson
Fixes: 4e3bb7e95fb72401119f4d095177fa6420e0e6e7
Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

Makefile
@@ -78,9 +78,6 @@ local-validate validate: install.tools ## validate DCO and gofmt on the host
 install.tools:
 	$(MAKE) -C tests/tools
 
-$(FFJSON):
-	$(MAKE) -C tests/tools
-
 install.docs: docs
 	$(MAKE) -C docs install
 