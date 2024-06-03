containers/storage

commit 6875c9fbcf1cb6e06ee5e363982a059adec9693b

chunked: ignore the tar-split data if digest is empty
if a digest was not specified in the TOC, ignore completely the
tar-split data.

Otherwise the clients fail to pull images created before commit
b5413c2bd64742a08384300f09616a66231b21d1.

Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

pkg/chunked/compression_linux.go
@@ -210,14 +210,18 @@ func readZstdChunkedManifest(blobStream ImageSourceSeekable, tocDigest digest.Di
 
 	decodedTarSplit := []byte{}
 	if tarSplitChunk.Offset > 0 {
-		tarSplit, err := readBlob(tarSplitChunk.Length)
-		if err != nil {
-			return nil, nil, nil, 0, err
-		}
+		tarSplitDigest := toc.TarSplitDigest.String()
+		// ignore the tar-split data if the digest was not specified
+		if tarSplitDigest != "" {
+			tarSplit, err := readBlob(tarSplitChunk.Length)
+			if err != nil {
+				return nil, nil, nil, 0, err
+			}
 
-		decodedTarSplit, err = decodeAndValidateBlob(tarSplit, tarSplitLengthUncompressed, toc.TarSplitDigest.String())
-		if err != nil {
-			return nil, nil, nil, 0, fmt.Errorf("validating and decompressing tar-split: %w", err)
+			decodedTarSplit, err = decodeAndValidateBlob(tarSplit, tarSplitLengthUncompressed, toc.TarSplitDigest.String())
+			if err != nil {
+				return nil, nil, nil, 0, fmt.Errorf("validating and decompressing tar-split: %w", err)
+			}
 		}
 	}
 	return decodedBlob, toc, decodedTarSplit, int64(manifestChunk.Offset), err

commit 4595fa2aab6528b0f4a9dd306a5f66b7b58fab8f

chunked: fix deadlock by always consuming tar-split
always consume the tar-split data when present to avoid blocking the
producer. Previously, the tar-split data was only read when the digest
was specified.

commit 6875c9fbcf1cb6e06ee5e363982a059adec9693b introduced the
regression.

Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

pkg/chunked/compression_linux.go
@@ -210,14 +210,13 @@ func readZstdChunkedManifest(blobStream ImageSourceSeekable, tocDigest digest.Di
 
 	decodedTarSplit := []byte{}
 	if tarSplitChunk.Offset > 0 {
-		tarSplitDigest := toc.TarSplitDigest.String()
-		// ignore the tar-split data if the digest was not specified
-		if tarSplitDigest != "" {
-			tarSplit, err := readBlob(tarSplitChunk.Length)
-			if err != nil {
-				return nil, nil, nil, 0, err
-			}
-
+		// we must consume the data to not block the producer
+		tarSplit, err := readBlob(tarSplitChunk.Length)
+		if err != nil {
+			return nil, nil, nil, 0, err
+		}
+		// but ignore it when the digest is not present, because we can’t authenticate it against tocDigest
+		if toc.TarSplitDigest != "" {
 			decodedTarSplit, err = decodeAndValidateBlob(tarSplit, tarSplitLengthUncompressed, toc.TarSplitDigest.String())
 			if err != nil {
 				return nil, nil, nil, 0, fmt.Errorf("validating and decompressing tar-split: %w", err)