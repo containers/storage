containers/storage

commit 48e010f485ffe0dabc8a4e6092dfba2fc74f59c3

Fix errcheck: error return value of `(*os/exec.Cmd).Run` is not checked
Signed-off-by: Jan Rodák <hony.com@seznam.cz>

drivers/aufs/aufs.go
@@ -188,7 +188,9 @@ func Init(home string, options graphdriver.Options) (graphdriver.Driver, error) 
 func supportsAufs() error {
 	// We can try to modprobe aufs first before looking at
 	// proc/filesystems for when aufs is supported
-	exec.Command("modprobe", "aufs").Run()
+	if err := exec.Command("modprobe", "aufs").Run(); err != nil {
+		logrus.Warnf("Execution of `modprobe aufs` ended with error: %v", err)
+	}
 
 	if unshare.IsRootless() {
 		return ErrAufsNested

drivers/overlay/overlay.go
@@ -692,7 +692,9 @@ func supportsOverlay(home string, homeMagic graphdriver.FsMagic, rootUID, rootGI
 
 	selinuxLabelTest := selinux.PrivContainerMountLabel()
 
-	exec.Command("modprobe", "overlay").Run()
+	if err := exec.Command("modprobe", "overlay").Run(); err != nil {
+		logrus.Warnf("Execution of `modprobe overlay` ended with error: %v", err)
+	}
 
 	logLevel := logrus.ErrorLevel
 	if unshare.IsRootless() {

commit c114633c243ac53007048c3e39145842bff73a90

drivers: change verbosity to debug for modprobe failures
commit 48e010f485ffe0dabc8a4e6092dfba2fc74f59c3 caused the regression
with rootless since an unprivileged user cannot use `modprobe`.

Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

drivers/aufs/aufs.go
@@ -189,7 +189,7 @@ func supportsAufs() error {
 	// We can try to modprobe aufs first before looking at
 	// proc/filesystems for when aufs is supported
 	if err := exec.Command("modprobe", "aufs").Run(); err != nil {
-		logrus.Warnf("Execution of `modprobe aufs` ended with error: %v", err)
+		logrus.Debugf("Execution of `modprobe aufs` ended with error: %v", err)
 	}
 
 	if unshare.IsRootless() {

drivers/overlay/overlay.go
@@ -693,7 +693,7 @@ func supportsOverlay(home string, homeMagic graphdriver.FsMagic, rootUID, rootGI
 	selinuxLabelTest := selinux.PrivContainerMountLabel()
 
 	if err := exec.Command("modprobe", "overlay").Run(); err != nil {
-		logrus.Warnf("Execution of `modprobe overlay` ended with error: %v", err)
+		logrus.Debugf("Execution of `modprobe overlay` ended with error: %v", err)
 	}
 
 	logLevel := logrus.ErrorLevel