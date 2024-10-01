7abefb584a3447882eb9332414d2be9f90942624
Jan Rodák <hony.com@seznam.cz>
2024-10-01 17:54:08 +0200

    Fix git-validation.sh to run in cirrus CI
    Signed-off-by: Jan Rodák <hony.com@seznam.cz>

--- hack/git-validation.sh
+++ hack/git-validation.sh
@@ -5,5 +5,9 @@ if [ ! -x "$GIT_VALIDATION" ]; then
 	echo Try installing it with \"make install.tools\"
 	exit 1
 fi
-EPOCH_TEST_COMMIT=$(git merge-base ${DEST_BRANCH:-main} HEAD)
+
+EPOCH_TEST_COMMIT=$CIRRUS_BASE_SHA
+if [ -z "${EPOCH_TEST_COMMIT}" ]; then
+	EPOCH_TEST_COMMIT=$(git merge-base ${DEST_BRANCH:-main} HEAD)
+fi
 exec "$GIT_VALIDATION" -q -run DCO,short-subject -range "${EPOCH_TEST_COMMIT}..HEAD"
