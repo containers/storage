3eac9030ffd275c6729ea230e1056a4b84e3eae3
Han-Wen Nienhuys <hanwen@engflow.com>
2025-03-03 10:09:32 +0100

    pkg/archive: drop gratuitious writes to TarOptions.ExcludePatterns.
    A nil and empty slice are equivalent, except resulting JSON
    serialization.  There is JSON serialization in applyLayerHandler(),
    but it's internal to the storage package and therefore not subject to
    compatibility promises.
    
    Signed-off-by: Han-Wen Nienhuys <hanwen@engflow.com>

--- pkg/archive/archive.go
+++ pkg/archive/archive.go
@@ -1216,9 +1216,6 @@ func untarHandler(tarArchive io.Reader, dest string, options *TarOptions, decomp
 	if options == nil {
 		options = &TarOptions{}
 	}
-	if options.ExcludePatterns == nil {
-		options.ExcludePatterns = []string{}
-	}
 
 	r := tarArchive
 	if decompress {
