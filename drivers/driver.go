6e95dfc6dbe83f3834c2c488149488863f425cdd
Giuseppe Scrivano <gscrivan@redhat.com>
2025-05-07 08:22:13 +0200

    chunked: clarify TarSplit ownership
    The comment for the `TarSplit` field in `DriverWithDifferOutput`
    was updated to make its ownership and the responsibility for
    closing the file descriptor explicit.
    
    Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

--- drivers/driver.go
+++ drivers/driver.go
@@ -216,8 +216,10 @@ type DriverWithDifferOutput struct {
 	CompressedDigest   digest.Digest
 	Metadata           string
 	BigData            map[string][]byte
-	TarSplit           *os.File // nil if not available
-	TOCDigest          digest.Digest
+	// TarSplit is owned by the [DriverWithDifferOutput], and must be closed by calling one of
+	// [Store.ApplyStagedLayer]/[Store.CleanupStagedLayer].  It is nil if not available.
+	TarSplit  *os.File
+	TOCDigest digest.Digest
 	// RootDirMode is the mode of the root directory of the layer, if specified.
 	RootDirMode *os.FileMode
 	// Artifacts is a collection of additional artifacts
